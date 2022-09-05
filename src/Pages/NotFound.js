3301a642cf58189e225c73c54631b0491d30df16
Bahauddin-Fahad <bahauddin.fahad819@gmail.com>
2022-09-05 19:40:29 +0600

    animation added

--- src/Pages/NotFound.js
+++ src/Pages/NotFound.js
@@ -2,8 +2,8 @@ import React from "react";
 import Page404 from "../Assets/images/notFound/Page404.gif";
 const NotFound = () => {
   return (
-    <div className=" bg-white">
-      <img src={Page404} alt="" />
+    <div className="bg-white">
+      <img className="mx-auto" src={Page404} alt="" />
     </div>
   );
 };
